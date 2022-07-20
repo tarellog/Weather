ba41213445c5132334572be08dc624dd608e50a8
Miri <azer535@gmail.com>
2022-07-20 11:46:03 +0300

    Add models
    Edit ListWeatherModel. Add MainWeatherModel, WeatherInfoModel, CloudsModel, WindModel, SysModel.

--- app/src/main/java/com/example/weatherapp/data/model/ListWeatherModel.kt
+++ app/src/main/java/com/example/weatherapp/data/model/ListWeatherModel.kt
@@ -5,4 +5,20 @@ import com.google.gson.annotations.SerializedName
 data class ListWeatherModel(
     @SerializedName("id")
     val id: Int,
+    @SerializedName("main")
+    val main: MainWeatherModel,
+    @SerializedName("weather")
+    val weather: List<WeatherInfoModel>,
+    @SerializedName("clouds")
+    val clouds: CloudsModel,
+    @SerializedName("wind")
+    val wind: WindModel,
+    @SerializedName("visibility")
+    val visibility: Int,
+    @SerializedName("pop")
+    val pop: Int,
+    @SerializedName("sys")
+    val sys: SysModel,
+    @SerializedName("dt_txt")
+    val dt_txt: String
 )
