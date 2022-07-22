627f393a772f55574b1444d8d218941b59aac412
Miri <azer535@gmail.com>
2022-07-22 11:30:29 +0300

    Edit view holder
    Add binding HeaderScreenWeatherHolder.

--- app/src/main/java/com/example/weatherapp/ui/recycler/HeaderScreenWeatherHolder.kt
+++ app/src/main/java/com/example/weatherapp/ui/recycler/HeaderScreenWeatherHolder.kt
@@ -1,4 +1,19 @@
 package com.example.weatherapp.ui.recycler
 
-class HeaderScreenWeatherHolder {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp.data.model.custommodel.TodayWeatherModel
+import com.example.weatherapp.databinding.HeaderItemBinding
+
+class HeaderScreenWeatherHolder(
+    private val binding: HeaderItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+    constructor(parent: ViewGroup) : this(
+        HeaderItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
+
+    fun populate() {
+
+    }
 }
