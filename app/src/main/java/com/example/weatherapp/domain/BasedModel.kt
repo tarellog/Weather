1ea0bac2568f6fb5103c058ed150365c89e05dce
Miri <azer535@gmail.com>
2022-07-23 22:57:57 +0300

    Rename BasedModel
    Move BasedModel to sealed class.

--- app/src/main/java/com/example/weatherapp/domain/BasedModel.kt
+++ app/src/main/java/com/example/weatherapp/domain/BasedModel.kt
@@ -1,4 +1,21 @@
 package com.example.weatherapp.domain
 
-interface BasedModel {
+import java.util.*
+
+sealed class BasedModel {
+
+    data class DailyWeatherModel(
+        val date: Date,
+        val minTemp: Int,
+        val maxTemp: Int,
+        val icon: String
+    ) : BasedModel()
+
+    data class TodayWeatherModel(
+        val date: Date,
+        val icon: String,
+        val temp: Int,
+        val description: String
+    ) : BasedModel()
+
 }
