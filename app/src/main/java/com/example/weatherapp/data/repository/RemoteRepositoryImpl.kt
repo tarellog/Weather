92bcc931c6f428e622a1e4f2e3ee54a558274d70
Miri <azer535@gmail.com>
2022-07-24 23:19:41 +0300

    Fix class RemoteRepositoryImpl
    Add function parameter. Swap return WeatherModel to BasedModel and changed the logic, add map.

--- app/src/main/java/com/example/weatherapp/data/repository/RemoteRepositoryImpl.kt
+++ app/src/main/java/com/example/weatherapp/data/repository/RemoteRepositoryImpl.kt
@@ -1,15 +1,12 @@
 package com.example.weatherapp.data.repository
 
-import android.util.Log
-import com.example.weatherapp.data.model.ListWeatherModel
-import com.example.weatherapp.data.model.WeatherModel
+import com.example.weatherapp.domain.BasedModel
 import com.example.weatherapp.domain.RemoteRepository
+import com.example.weatherapp.domain.mapToDisplayModel
+import com.example.weatherapp.domain.mapToHeaderDisplayModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -24,9 +21,16 @@ class RemoteRepositoryImpl : RemoteRepository {
 
     private val apiService: ApiWeatherService = retrofit.create(ApiWeatherService::class.java)
 
-    override fun requestRepository(): Single<WeatherModel> {
-        return apiService.getApi()
+    override fun requestRepository(cityName: String): Single<List<BasedModel>> {
+        return apiService.getApi(cityName)
+            .map { weatherModel ->
+                buildList {
+                    add(weatherModel.list.mapToHeaderDisplayModel())
+                    addAll(weatherModel.list.mapToDisplayModel())
+                }
+            }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
     }
+
 }
