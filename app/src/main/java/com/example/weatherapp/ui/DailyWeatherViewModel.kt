32ed0c5fa2dfac9a9907124393b4738fe76ea2a2
Miri <azer535@gmail.com>
2022-07-25 01:09:31 +0300

    Fix ViewModel
    Fixed implementation of logic display screen. Add sealed class ViewState.

--- app/src/main/java/com/example/weatherapp/ui/DailyWeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/DailyWeatherViewModel.kt
@@ -3,33 +3,32 @@ package com.example.weatherapp.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.weatherapp.data.model.custommodel.DailyWeatherModel
-import com.example.weatherapp.data.model.custommodel.TodayWeatherModel
 import com.example.weatherapp.data.repository.RemoteRepositoryImpl
 import com.example.weatherapp.domain.BasedModel
 import com.example.weatherapp.domain.RemoteRepository
-import com.example.weatherapp.domain.mapToDisplayModel
-import com.example.weatherapp.domain.mapToHeaderDisplayModel
 
 class DailyWeatherViewModel : ViewModel() {
 
+    sealed class ViewState {
+        data class Success(
+            val weather: List<BasedModel>
+        ) : ViewState()
+
+        object Error : ViewState()
+    }
+
     private val repository: RemoteRepository = RemoteRepositoryImpl()
 
-    private var _dailyWeatherModel = MutableLiveData<List<DailyWeatherModel>>()
-    val dailyWeatherModel: LiveData<List<DailyWeatherModel>> get() = _dailyWeatherModel
+    private var _basedModel = MutableLiveData<ViewState>()
+    val basedModel: LiveData<ViewState> get() = _basedModel
 
-    private var _todayWeatherModel = MutableLiveData<List<TodayWeatherModel>>()
-    val todayWeatherModel: LiveData<List<TodayWeatherModel>> get() = _todayWeatherModel
-
-    fun loadTodayWeatherData() {
-        repository.requestRepository()
-            .subscribe({ _todayWeatherModel.postValue(it.list.mapToHeaderDisplayModel()) }) {}
+    fun loadBasedWeatherData(cityName: String) {
+        repository.requestRepository(cityName)
+            .subscribe({ listWeatherModel ->
+                _basedModel.postValue(ViewState.Success(listWeatherModel))
+            }, {
+                _basedModel.postValue(ViewState.Error)
+            })
     }
-
-    fun loadDailyWeatherData() {
-        repository.requestRepository()
-            .subscribe({ _dailyWeatherModel.postValue(it.list.mapToDisplayModel()) }) {}
-    }
-
 
 }
