cd125dff6fb47df16f1377f3e256e016a80e30e0
Miri <azer535@gmail.com>
2022-07-25 00:58:23 +0300

    Fix api service
    Add parameter in method annotation Query

--- app/src/main/java/com/example/weatherapp/data/repository/ApiWeatherService.kt
+++ app/src/main/java/com/example/weatherapp/data/repository/ApiWeatherService.kt
@@ -3,9 +3,15 @@ package com.example.weatherapp.data.repository
 import com.example.weatherapp.data.model.WeatherModel
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiWeatherService {
-    @GET("forecast?lat=52&lon=41&lang=ru&appid=296dbfe3af91642e2df4bf1e68064c26")
 
-    fun getApi(): Single<WeatherModel>
+    companion object {
+        const val CITY_NAME = "q"
+    }
+
+    @GET("forecast?lang=ru&appid=296dbfe3af91642e2df4bf1e68064c26")
+    fun getApi(@Query(CITY_NAME) cityName: String): Single<WeatherModel>
+
 }
