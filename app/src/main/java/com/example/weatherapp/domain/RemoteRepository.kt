d453c5cd8842462d35748c605092464492ae9d2b
Miri <azer535@gmail.com>
2022-07-24 23:15:28 +0300

    Fix interface RemoteRepository
    Add parameter. Swap return WeatherModel to BasedModel.

--- app/src/main/java/com/example/weatherapp/domain/RemoteRepository.kt
+++ app/src/main/java/com/example/weatherapp/domain/RemoteRepository.kt
@@ -1,8 +1,7 @@
 package com.example.weatherapp.domain
 
-import com.example.weatherapp.data.model.WeatherModel
 import io.reactivex.rxjava3.core.Single
 
 interface RemoteRepository {
-    fun requestRepository(): Single<WeatherModel>
+    fun requestRepository(cityName: String): Single<List<BasedModel>>
 }
