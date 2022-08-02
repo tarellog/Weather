56aa30d21c8760d73f0dfd8345b07544a635ba8f
Miri <azer535@gmail.com>
2022-08-02 18:11:41 +0300

    Fix module
    Transfer module Retrofit in package common.

--- app/src/main/java/com/example/weatherapp/weather/di/WeatherComponent.kt
+++ app/src/main/java/com/example/weatherapp/weather/di/WeatherComponent.kt
@@ -1,13 +1,19 @@
 package com.example.weatherapp.weather.di
 
 import com.example.weatherapp.common.FactoryViewModels
+import com.example.weatherapp.common.RetrofitModule
 import com.example.weatherapp.weather.domain.RemoteRepository
 import com.example.weatherapp.weather.ui.DailyWeatherFragment
 import com.example.weatherapp.weather.viewmodel.DailyWeatherViewModel
 import dagger.Component
 import javax.inject.Singleton
 
-@Component(modules = [WeatherModule::class])
+@Component(
+    modules = [
+        WeatherModule::class,
+        RetrofitModule::class
+    ]
+)
 @Singleton
 
 interface WeatherComponent {
--- app/src/main/java/com/example/weatherapp/weather/di/WeatherModule.kt
+++ app/src/main/java/com/example/weatherapp/weather/di/WeatherModule.kt
@@ -6,20 +6,10 @@ import com.example.weatherapp.weather.network.repository.RemoteRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 class WeatherModule {
-    @Provides
-    @Singleton
-    fun provideRetrofit(): Retrofit = Retrofit.Builder()
-            .baseUrl("http://api.openweathermap.org/data/2.5/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-            .build()
-
     @Provides
     @Singleton
     fun provideApiService(retrofit: Retrofit): ApiWeatherService =
