e75b94bf62f09af30dfe776d6be4e03b305e7813
Miri <azer535@gmail.com>
2022-08-09 12:30:32 +0300

    Add extension
    Add extension flow observe.

--- app/src/main/java/com/example/weatherapp/weather/domain/ExtensionStateFlow.kt
+++ app/src/main/java/com/example/weatherapp/weather/domain/ExtensionStateFlow.kt
@@ -1,3 +1,13 @@
 package com.example.weatherapp.weather.domain
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
+fun <T> Flow<T>.observe(
+    scope: CoroutineScope,
+    action: suspend (T) -> Unit,
+) = this
+    .onEach(action)
+    .launchIn(scope)
--- app/src/main/java/com/example/weatherapp/weather/ui/DailyWeatherFragment.kt
+++ app/src/main/java/com/example/weatherapp/weather/ui/DailyWeatherFragment.kt
@@ -1,6 +1,5 @@
 package com.example.weatherapp.weather.ui
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,20 +8,18 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.example.weatherapp.common.di.ViewModelFactory
 import com.example.weatherapp.databinding.FragmentDailyWeatherBinding
 import com.example.weatherapp.dialogweather.SearchDialogFragment
 import com.example.weatherapp.weather.adapter.dailyweather.DailyItem
 import com.example.weatherapp.weather.adapter.dailyweather.HeaderItem
+import com.example.weatherapp.weather.domain.observe
 import com.example.weatherapp.weather.viewmodel.DailyWeatherViewModel
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.adapters.GenericItemAdapter
 import com.mikepenz.fastadapter.adapters.ItemAdapter
 import com.mikepenz.fastadapter.diff.FastAdapterDiffUtil
-import kotlinx.coroutines.launch
 
 class DailyWeatherFragment : Fragment() {
     private var _binding: FragmentDailyWeatherBinding? = null
@@ -53,31 +50,23 @@ class DailyWeatherFragment : Fragment() {
         return binding.root
     }
 
-    @SuppressLint("UnsafeRepeatOnLifecycleDetector")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        lifecycleScope.launch {
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED){
-                launch {
-                    viewModel.message.collect{
-                        Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
-                    }
-                }
-                launch {
-                    viewModel.headerModel.collect {
-                        FastAdapterDiffUtil[headerAdapter] = it.map(::HeaderItem)
-                    }
-                }
-                launch {
-                    viewModel.dailyModel.collect {
-                        FastAdapterDiffUtil[itemAdapter] = it.map(::DailyItem)
-                    }
-                }
-                launch {
-                    viewModel.city.collect { binding.city.text = it }
-                }
-            }
-        }
+        viewModel.headerModel.observe(
+            lifecycleScope,
+            action = { FastAdapterDiffUtil[headerAdapter] = it.map(::HeaderItem) }
+        )
+        viewModel.dailyModel.observe(
+            lifecycleScope,
+            action = { FastAdapterDiffUtil[itemAdapter] = it.map(::DailyItem) }
+        )
+        viewModel.city.observe(
+            lifecycleScope,
+            action = { binding.city.text = it }
+        )
+        viewModel.message.observe(
+            lifecycleScope,
+            action = { Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show() }
+        )
     }
 }
