51225946aad02c2e2dc42d4c5453e0a78b257d44
Miri <azer535@gmail.com>
2022-08-24 11:01:13 +0300

    Add test use cases class WeatherLoaderImplTest

---
+++ app/src/test/java/com/example/weatherapp/weather/usecases/weatherloader/WeatherLoaderImplTest.kt
@@ -0,0 +1,52 @@
+package com.example.weatherapp.weather.usecases.weatherloader
+
+import com.example.weatherapp.weather.network.weatherrequest.RxImmediateSchedulerRule
+import com.example.weatherapp.weather.network.weatherrequest.weatherActualModel
+import com.example.weatherapp.weather.weatherModel
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class WeatherLoaderImplTest {
+    @Rule
+    @JvmField
+    var testSchedulerRule = RxImmediateSchedulerRule()
+
+    @Mock
+    lateinit var weatherRequest: WeatherService
+
+    lateinit var weatherLoader: WeatherLoader
+
+    @Before
+    fun setUp() {
+        weatherLoader = WeatherLoaderImpl(weatherRequest)
+    }
+
+    @Test
+    fun getWeatherTest() {
+        Mockito.`when`(weatherRequest.getWeather(weatherModel.cityName)).thenReturn(
+            Single.just(weatherActualModel))
+
+        weatherLoader.getWeather(weatherModel.cityName).test()
+            .assertNoErrors()
+            .assertValueCount(1)
+            .assertValues(weatherActualModel)
+            .assertComplete()
+    }
+
+    @Test
+    fun getWeatherExceptionTest() {
+        Mockito.`when`(weatherRequest.getWeather(weatherModel.cityName))
+            .thenReturn(Single.error(Throwable()))
+
+        weatherLoader.getWeather(weatherModel.cityName).test()
+            .assertValueCount(0)
+            .assertNotComplete()
+    }
+}
