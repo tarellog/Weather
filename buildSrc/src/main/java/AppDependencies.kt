e622708a9fde417f43dab0eef87d61c7df5befae
Miri <azer535@gmail.com>
2022-08-25 11:31:21 +0300

    Add commentary region to AppDependencies

--- buildSrc/src/main/java/AppDependencies.kt
+++ buildSrc/src/main/java/AppDependencies.kt
@@ -1,51 +1,59 @@
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
 object AppDependencies {
-    //std lib
-    val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
-
-    //android ui
+    //region android ui
+    private const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
     private val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
     private val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
     private val constraintLayout =
         "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
     private val material = "com.google.android.material:material:${Versions.material}"
+    //endregion
 
-    //Retrofit
+    //region Retrofit
     private val gson = "com.google.code.gson:gson:${Versions.gson}"
     private val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
     private val converterGson = "com.squareup.retrofit2:converter-gson:${Versions.converterGson}"
+    //endregion
 
-    //Lifecycle
+    //region Lifecycle
     private val fragmentKtx = "androidx.fragment:fragment-ktx:${Versions.frgamentKtx}"
     private val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.viewModelKtx}"
+    //endregion
 
-    //Navigation
+    //region Navigation
     private val navigation = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
     private val navigationUi = "androidx.navigation:navigation-ui-ktx:${Versions.navigationUi}"
+    //endregion
 
-    //FastAdapter
+    //region FastAdapter
     private val fastAdapter = "com.mikepenz:fastadapter:${Versions.fastAdapter}"
     private val fastAdapterBinding = "com.mikepenz:fastadapter-extensions-binding:${Versions.fastBinding}"
     private val fastAdapterDiff = "com.mikepenz:fastadapter-extensions-diff:${Versions.fastDiff}"
     private val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recycler}"
+    //endregion
 
-    //Dagger
+    //region Dagger
     private val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+    //endregion
 
-    //kapt
+    //region kapt
     private val daggerKapt = "com.google.dagger:dagger-compiler:2.41"
+    //endregion
 
-    //Rx
+    //region Rx
     private val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
     private val rxJava = "io.reactivex.rxjava2:rxjava:${Versions.rxJava}"
     private val rxRetrofit = "com.squareup.retrofit2:adapter-rxjava2:${Versions.rxRetrofit}"
+    //endregion
 
-    //test libs
+    //region test libs
     private val junit = "junit:junit:${Versions.junit}"
     private val extJUnit = "androidx.test.ext:junit:${Versions.extJunit}"
     private val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+    //endregion
 
+    //region appLibraries list
     val appLibraries = listOf(
         kotlinStdLib,
         coreKtx,
@@ -68,19 +76,26 @@ object AppDependencies {
         rxJava,
         rxRetrofit,
     )
+    //endregion
 
+    //region kapt list
     val kapt = listOf (
         daggerKapt
     )
+    //endregion
 
+    //region androidTestLibraries list
     val androidTestLibraries = listOf(
         extJUnit,
         espressoCore
     )
+    //endregion
 
+    //region testLibraries list
     val testLibraries = listOf(
         junit
     )
+    //endregion
 }
 
 //util functions for adding the different type dependencies from build.gradle file
