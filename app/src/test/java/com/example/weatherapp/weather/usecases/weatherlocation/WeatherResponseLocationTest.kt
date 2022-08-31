2db8126741f359dad129d3dac0b10b64208ed083
Miri <azer535@gmail.com>
2022-08-31 13:21:38 +0300

    Add use cases WeatherResponseLocationTest

---
+++ app/src/test/java/com/example/weatherapp/weather/usecases/weatherlocation/WeatherResponseLocationTest.kt
@@ -0,0 +1,61 @@
+package com.example.weatherapp.weather.usecases.weatherlocation
+
+import com.example.weatherapp.weather.usecases.common.RxImmediateSchedulerRule
+import com.example.weatherapp.weather.usecases.common.weatherActualModel
+import com.example.weatherapp.weather.usecases.common.weatherLocation
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class WeatherResponseLocationTest {
+    @Rule
+    @JvmField
+    var testSchedulerRule = RxImmediateSchedulerRule()
+
+    @Mock
+    lateinit var locationRequest: RequestLocation
+
+    @Mock
+    lateinit var locationService: ServiceLocation
+
+    lateinit var weatherResponseLocation: ResponseLocation
+
+    @Before
+    fun setUp() {
+        weatherResponseLocation = WeatherResponseLocation(locationRequest, locationService)
+    }
+
+    @Test
+    fun getWeatherTest() {
+        Mockito.`when`(locationRequest.getWeatherLocation(weatherLocation)).thenReturn(
+            Single.just(weatherActualModel))
+
+        Mockito.`when`(locationService.getLocation()).thenReturn(
+            Single.just(weatherLocation))
+
+        weatherResponseLocation.getWeatherByLocation().test()
+            .assertNoErrors()
+            .assertValueCount(1)
+            .assertValues(weatherActualModel)
+            .assertComplete()
+    }
+
+    @Test
+    fun getWeatherExceptionTest() {
+        Mockito.`when`(locationRequest.getWeatherLocation(weatherLocation))
+            .thenReturn(Single.error(Throwable()))
+
+        Mockito.`when`(locationService.getLocation()).thenReturn(
+            Single.just(weatherLocation))
+
+        weatherResponseLocation.getWeatherByLocation().test()
+            .assertValueCount(0)
+            .assertNotComplete()
+    }
+}
