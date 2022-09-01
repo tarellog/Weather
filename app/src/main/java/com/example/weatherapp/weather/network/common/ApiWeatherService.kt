80d6c7b86ee83d07fbdc41db239812a26dded6d7
Miri <azer535@gmail.com>
2022-09-01 17:00:46 +0300

    Refactoring const val

--- app/src/main/java/com/example/weatherapp/weather/network/common/ApiWeatherService.kt
+++ app/src/main/java/com/example/weatherapp/weather/network/common/ApiWeatherService.kt
@@ -5,14 +5,11 @@ import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+private const val CITY_NAME = "q"
+private const val LATITUDE = "lat"
+private const val LONGITUDE = "lon"
+
 interface ApiWeatherService {
-
-    companion object {
-        const val CITY_NAME = "q"
-        const val LATITUDE = "lat"
-        const val LONGITUDE = "lon"
-    }
-
     @GET("forecast?lang=ru&units=metric&appid=296dbfe3af91642e2df4bf1e68064c26")
     fun getApi(@Query(CITY_NAME) cityName: String): Single<WeatherModel>
     @GET("forecast?lang=ru&units=metric&appid=296dbfe3af91642e2df4bf1e68064c26")
@@ -20,5 +17,4 @@ interface ApiWeatherService {
         @Query(LATITUDE) latitude: Double,
         @Query(LONGITUDE) longitude: Double
     ): Single<WeatherModel>
-
 }
