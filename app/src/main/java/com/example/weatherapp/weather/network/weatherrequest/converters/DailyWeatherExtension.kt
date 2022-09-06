bb95b2574316071ab32a174b9b2da00eedcd7b35
Miri <azer535@gmail.com>
2022-09-06 14:36:45 +0300

    Add groupBy

--- app/src/main/java/com/example/weatherapp/weather/network/weatherrequest/converters/DailyWeatherExtension.kt
+++ app/src/main/java/com/example/weatherapp/weather/network/weatherrequest/converters/DailyWeatherExtension.kt
@@ -8,29 +8,22 @@ import java.util.*
 private val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
 
 fun List<ListWeatherModel>.mapToDisplayModel(): List<DailyWeather> {
-    var currentDay = Date()
     return this
+        .groupBy { it.toDailyWeather().date.day}
         .map {
-            val hours = this.mapToHoursDisplayModel(it.toDailyWeather().date)
+            val date = simpleDateFormat.parse(it.value.first().dt_txt)
+            val icon = it.value.first().weather.first().icon
+            val hours = this.mapToHoursDisplayModel(it.value.first().toDailyWeather().date)
             val maxTemp = hours.map { it1 -> it1.tempHours }.maxOrNull()?.toInt() ?: 0
             val minTemp = hours.map { it1 -> it1.tempHours }.minOrNull()?.toInt() ?: 0
-            DailyWeather(it.toDailyWeather().date, maxTemp, minTemp, it.toDailyWeather().icon, hours)
-        }
-        .filterIndexed { index, dailyWeatherModel ->
-            if (index == 0) {
-                currentDay = dailyWeatherModel.date
-                true
-            } else if (dailyWeatherModel.date.after(currentDay)) {
-                currentDay = dailyWeatherModel.date
-                true
-            } else false
+            DailyWeather(date, maxTemp, minTemp, icon, hours)
         }
 }
 
 fun ListWeatherModel.toDailyWeather() = DailyWeather(
     date = simpleDateFormat.parse(dt_txt)!!,
-    minTemp = listOf(this).map { it.toTimeWeather().tempHours }.minOrNull()?.toInt() ?: 0,
-    maxTemp = listOf(this).map { it.toTimeWeather().tempHours }.maxOrNull()?.toInt() ?: 0,
+    minTemp = listOf(this.main.temp_min).minOrNull()?.toInt() ?: 0,
+    maxTemp = listOf(this.main.temp_max).maxOrNull()?.toInt() ?: 0,
     icon = weather.first().icon,
-    hoursList = listOf(this).mapToHoursDisplayModel(simpleDateFormat.parse(dt_txt)!!)
+    hoursList = listOf(toTimeWeather())
 )
