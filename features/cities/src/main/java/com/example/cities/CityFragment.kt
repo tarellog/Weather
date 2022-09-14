2d2a06e74d49c0cc4232867bf08fa3cf8e58e702
Miri <azer535@gmail.com>
2022-09-14 17:42:40 +0300

    Add binding to city fragment

--- features/cities/src/main/java/com/example/cities/CityFragment.kt
+++ features/cities/src/main/java/com/example/cities/CityFragment.kt
@@ -5,14 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.cities.databinding.FragmentCityBinding
 
 class CityFragment : Fragment() {
+    private var _binding: FragmentCityBinding? = null
+    private val binding get() = _binding ?: throw NullPointerException("Binding is not initialized")
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_city, container, false)
-    }
+    ): View {
+        _binding = FragmentCityBinding.inflate(inflater, container, false)
 
+        return binding.root
+    }
 }
