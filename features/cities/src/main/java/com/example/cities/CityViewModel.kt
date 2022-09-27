bd5f48cc1fec390413cc2b2ca269b2173b55acb2
Miri <azer535@gmail.com>
2022-09-27 15:26:12 +0300

    Add event message error

--- features/cities/src/main/java/com/example/cities/CityViewModel.kt
+++ features/cities/src/main/java/com/example/cities/CityViewModel.kt
@@ -1,10 +1,16 @@
 package com.example.cities
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class CityViewModel : ViewModel() {
+    private var _messageError = MutableSharedFlow<Int>(0, 1, BufferOverflow.DROP_OLDEST)
+    val messageError get() = _messageError.asSharedFlow()
+
     private val _citiesList = MutableStateFlow<List<CityElement>>(emptyList())
     val citiesList get() = _citiesList.asStateFlow()
 
