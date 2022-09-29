b066b01c06b5c893d0a15135633276b4b740454d
Miri <azer535@gmail.com>
2022-09-29 17:20:44 +0300

    Add provider for navigationCommand

--- app/src/main/java/com/example/weatherapp/navigation/NavigationModule.kt
+++ app/src/main/java/com/example/weatherapp/navigation/NavigationModule.kt
@@ -1,8 +1,11 @@
 package com.example.weatherapp.navigation
 
+import com.example.weather.navigation.NavCommand
 import com.example.weather.navigation.WeatherNavigationProvider
 import dagger.Module
 import dagger.Provides
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import javax.inject.Singleton
 
 @Module
@@ -11,4 +14,14 @@ class NavigationModule {
     @Singleton
     fun providesNavigationScreen(): WeatherNavigationProvider =
         WeatherNavigationProviderImpl()
+
+    @Provides
+    @Singleton
+    fun navigationCommand(): MutableSharedFlow<NavCommand> {
+        return MutableSharedFlow(
+            replay = 0,
+            extraBufferCapacity = 1,
+            BufferOverflow.DROP_OLDEST
+        )
+    }
 }
