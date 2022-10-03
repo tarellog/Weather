d3c48f313fcc24117f6bac446b85d9671fe6c21c
Miri <azer535@gmail.com>
2022-10-03 15:57:37 +0300

    Add implementation to build.gradle core

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -36,9 +37,8 @@ android {
 }
 
 dependencies {
-
     implementation(AppDependencies.appLibraries)
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    testImplementation(AppDependencies.testLibraries)
+    androidTestImplementation(AppDependencies.androidTestLibraries)
+    kapt(AppDependencies.kapt)
 }
