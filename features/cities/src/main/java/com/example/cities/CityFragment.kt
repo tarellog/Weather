1515eabfd20fef29c0cf0f8108a1b0c97129b70c
Miri <azer535@gmail.com>
2022-10-07 16:28:53 +0300

    Add fake data

--- features/cities/src/main/java/com/example/cities/CityFragment.kt
+++ features/cities/src/main/java/com/example/cities/CityFragment.kt
@@ -1,16 +1,22 @@
 package com.example.cities
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.cities.databinding.FragmentCityBinding
 import com.example.cities.dimodule.CityComponentHolder
+import com.example.core.flow.observe
 
 class CityFragment : Fragment() {
     private var _binding: FragmentCityBinding? = null
     private val binding get() = _binding ?: throw NullPointerException("Binding is not initialized")
+
+    private val viewModel: CityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         CityComponentHolder
@@ -26,13 +32,17 @@ class CityFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentCityBinding.inflate(inflater, container, false)
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        viewModel.citiesList.observe(
+            viewLifecycleOwner.lifecycleScope,
+            action = { it },
+            onError = { Log.e("log", "error", it) }
+        )
+        viewModel.loadData()
     }
 }
